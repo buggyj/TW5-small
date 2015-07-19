19c30ce17ed6e841581178562095eb0d6dd892cd
buggyj <buggyjef@gmail.com>
2015-07-19 20:28:04 +0100

    inital msg support plugin

---
+++ msg/aamsgwidget.js
@@ -0,0 +1,127 @@
+/*\
+title: $:/bj/modules/widgets/msgwidget.js
+type: application/javascript
+module-type: widget
+
+Widget base class
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+/*
+Create a widget object as a basic for message widgets
+/*
+Add a list of event listeners from an array [{type:,handler:},...]
+*/
+Widget.prototype.addIdEventListeners = function(listeners) {
+	var self = this;
+	$tw.utils.each(listeners,function(listenerInfo) {
+		self.addIdEventListener(listenerInfo.id,listenerInfo.type,listenerInfo.handler);
+	});
+};
+/*
+Add a message event listener
+*/
+//bj should throw an error if action[id+'/'+type] already exists - should be unque otherwise it will cause an error
+Widget.prototype.addIdEventListener = function(id,type,handler) {
+	var self = this;
+	var newitem ={name:null,handle:null,next:null}
+
+	if(typeof handler === "string") { // The handler is a method name on this widget
+		newitem.handle= function(event) {
+			return self[handler].call(self,event);
+		};
+		newitem.name = handler; //save name so we can del later
+	} else { // The handler is a function
+		newitem.handle = function(event) {
+			return handler.call(self,event);
+		};
+	}
+	if (!action[id+'/'+type]) {
+		action[id+'/'+type] = newitem;
+	} else {
+		var next = action[id+'/'+type];
+		    while (next.next) next = next.next;
+		    next.next = newitem;
+	}
+	
+};
+
+Widget.prototype.delIdEventListeners = function(listeners) {
+	var self = this;
+	//action = {}; return;
+	$tw.utils.each(listeners,function(listenerInfo) {
+		self.delIdEventListener(listenerInfo.id,listenerInfo.type,listenerInfo.handler);
+	});
+};
+
+Widget.prototype.delIdEventListener = function(id,type,handler) {
+	var self = this;
+
+	if (!action[id+'/'+type]) { alert ("no entry found")
+		return;
+	} else {
+		var next = action[id+'/'+type];
+		if (typeof handler === "string") {alert ("str"+handler)
+			if (handler == next.name) {
+				 action[id+'/'+type] = next.next;
+				return;
+			} else {
+				while (next.next) {
+					if (handler == next.next.name) break;
+					next = next.next;
+				}
+				if (!next.next) {alert("not found str");return;}
+
+				next.next = next.next.next;
+				return;				
+
+			}
+		} else 	{
+			if (handler == next.handle) {
+				action[id+'/'+type] = next.next;
+				return;
+			} else {
+				while (next.next) {
+					if (handler == next.next.handle) break;
+					next = next.next;
+				}
+				if (!next.next) {alert("not found ");return;}
+			
+				next.next = next.next.next;
+				return;				
+
+			}
+		} 
+	}
+	
+};
+
+var actions = {};//global actions table
+
+
+/*
+Dispatch an event to a widget. If the widget doesn't handle the event then it is also dispatched to the parent widget
+*/
+Widget.prototype.dispatchIdEvent = function(id, event) {
+	var listener = action[id+'/'+ event.type];
+	while (listener) {
+		listener.handle(event);//alert("LISTEN")
+		
+		if(!listener.next) {
+			return true;
+		}
+		listener = listener.next
+	}
+	return true;
+};
+var action={};
+
+exports.msgwidget = Widget;
+
+})();

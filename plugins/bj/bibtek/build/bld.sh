8c0fc168bb3942115016f774b5984e57b860a079
buggyj <buggyjef@gmail.com>
2015-12-09 18:50:57 +0000

    all: changed file structure

--- plugins/bj/bibtek/build/bld.sh
+++ plugins/bj/bibtek/build/bld.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Set up the build output directory
-
 if [  -z "$TW5_BUILD_OUTPUT" ]; then
     TW5_BUILD_OUTPUT=.
 fi
@@ -10,8 +9,19 @@ if [  ! -d "$TW5_BUILD_OUTPUT" ]; then
     TW5_BUILD_OUTPUT=.
 fi
 
+# and the root of tw5 
+if [  -z "$TW5_ROOT" ]; then
+    TW5_ROOT=../../../../../../..
+fi
 
-node ../../../../../tiddlywiki.js \
+if [  ! -d "$TW5_ROOT" ]; then
+    TW5_ROOT=../../../../../../..
+fi
+
+# add path to root of plugin
+export TIDDLYWIKI_PLUGIN_PATH="${PWD%/*/*/*}:$TIDDLYWIKI_PLUGIN_PATH"
+
+node $TW5_ROOT/tiddlywiki.js \
 	./demo \
 	--verbose \
 	--rendertiddler $:/core/save/all $TW5_BUILD_OUTPUT/bibtek.html text/plain \
